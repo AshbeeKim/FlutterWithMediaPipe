d0e47a50150fae7a86368a414e6b75f137820aaa
Ashbee <ksb.forest@gmail.com>
2023-01-21 11:36:47 +0900

    fix: ScreenUtil -> ScreenUtilInit
    * versions

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        builder: () => MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'Flutter with Mediapipe',
-              theme: ThemeData(
-                appBarTheme: const AppBarTheme(
-                  elevation: 0.0,
-                  color: Colors.transparent,
-                ),
-              ),
-              home: const HomePage(),
-            ));
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter with Mediapipe',
+          theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+              elevation: 0.0,
+              color: Colors.transparent,
+            ),
+          ),
+          home: child,
+        );
+      },
+      child: const HomePage(),
+    );
   }
 }
